ab7c91b9bb4a513af4ff5644f858ced9dc8b62e3
spect0x4552 <valdisreturns@proton.me>
2024-10-28 23:30:38 +0200

    socket config

--- Networking/simple_socket.cpp
+++ Networking/simple_socket.cpp
@@ -1,5 +1,8 @@
-//
-// Created by spect0x4552 on 10/28/24.
-//
+//spect0x4552
+
 
 #include "simple_socket.hpp"
+
+HDE::simplesocket::simplesocket(int domain, int service, int protocol) {
+    int connection = socket(domain, service, protocol); // just for reference, not for user manipulation
+}
--- Networking/simple_socket.hpp
+++ Networking/simple_socket.hpp
@@ -2,7 +2,24 @@
 // Created by spect0x4552 on 10/28/24.
 //
 
-#ifndef SIMPLE_SOCKET_H
-#define SIMPLE_SOCKET_H
+#ifndef simple_socket_hpp
+#define simple_socket_hpp
 
-#endif //SIMPLE_SOCKET_H
+
+
+#include <stdio.h>
+#include <sys/socket.h>
+
+
+namespace HDE {
+    class simplesocket {
+
+    private:
+        int connection;
+    public:
+        simplesocket(int domain, int service, int protocol);
+    };
+
+}
+
+#endif
