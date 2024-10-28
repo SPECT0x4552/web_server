18bdb68594d5711fe2af6f3b40b5cc1d8bc1e2e8
spect0x4552 <valdisreturns@proton.me>
2024-10-29 01:05:45 +0200

    binding socket finished, some minor changes for the simple_socket files

---
+++ Networking/binding_socket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by spect0x4552 on 10/29/24.
+//
+
+#include "binding_socket.hpp"
---
+++ Networking/binding_socket.hpp
@@ -0,0 +1,16 @@
+//
+// Created by spect0x4552 on 10/29/24.
+//
+
+#ifndef BINDING_SOCKET_HPP
+#define BINDING_SOCKET_HPP
+
+
+
+class binding_socket {
+
+};
+
+
+
+#endif //BINDING_SOCKET_HPP
