38855f2175c397bba795ce335cab5895a389ae75
spect0x4552 <valdisreturns@proton.me>
2024-10-29 01:26:04 +0200

    connecting socket finished

--- Networking/connecting_socket.cpp
+++ Networking/connecting_socket.cpp
@@ -7,3 +7,16 @@
 
 
 // constructor
+
+
+HDE::connecting_socket::connecting_socket(int domain, int service, int protocol,
+    int port, u_long interface) : simple_socket(domain, service, protocol,
+        port,interface) {
+        set_connection(init_connection(get_sock(), get_address()));
+        test_connection(get_connection());
+}
+
+// definition of init_connection() virtual function
+int HDE::connecting_socket::init_connection(int sock, struct sockaddr address) {
+    return connect(sock, (struct sockaddr*)&address, sizeof(address));
+}
